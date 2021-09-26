0f8f2ffaf6a6918de0dab8ffa1866526f0a4e8c6
Mario <72976464+decisoft@users.noreply.github.com>
2021-09-26 17:17:53 +0200

    update

--- raspberry_setup.sh
+++ raspberry_setup.sh
@@ -12,6 +12,8 @@ apt-get install -y python3 python3-pip
 
 pip3 install docker-compose
 
+systemctl unmask systemd-logind
+
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
