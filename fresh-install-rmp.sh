dc34eddf121354efe99e14e7ea6cf562a693c6a9
Decisoft <72976464+decisoft@users.noreply.github.com>
2021-12-05 15:35:09 +0100

    Create fresh-install-rmp.sh

---
+++ fresh-install-rmp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo ¡Hola, Mario!
+
+echo Vamos a reinstalar todos tus programas y repositorios para dejarlo como antes en esta nueva instalación de Fedora
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+dnf -y update
+dnf -y install neofetch git curl wget zsh xclip xsel
+
+echo ** Firefox ** && flatpak install flathub org.mozilla.firefox && echo ¡Listo!
+
+echo ** Telegram ** && flatpak install flathub org.telegram.desktop && echo ¡Listo!
+
+echo ** Signal ** && flatpak install flathub org.signal.Signal && echo ¡Listo!
+
+echo ** OnlyOffice ** && flatpak install flathub org.onlyoffice.desktopeditors && echo ¡Listo!
+
+echo ** Calibre ** && flatpak install flathub com.calibre_ebook.calibre && echo ¡Listo!
+
+echo ** Discord ** && flatpak install flathub com.discordapp.Discord && echo ¡Listo!
+
+echo ** PDF Tricks ** && flatpak install flathub com.github.muriloventuroso.pdftricks && echo ¡Listo!
+
+echo ** VLC ** && flatpak install flathub org.videolan.VLC && echo ¡Listo!
+
+echo ** Typora ** && flatpak install flathub io.typora.Typora && echo ¡Listo!
+
+echo ** Byte ** && flatpak install flathub com.github.alainm23.byte && echo ¡Listo!
+
+echo ** Ungoogled Chromium ** && flatpak install flathub com.github.Eloston.UngoogledChromium && echo ¡Listo!
