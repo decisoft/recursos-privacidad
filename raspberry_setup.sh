c0edb203d64e57f2aa5ffcb29e156cf54ab58164
Decisoft <72976464+decisoft@users.noreply.github.com>
2022-01-02 18:35:43 +0000

    Update raspberry_setup.sh

--- raspberry_setup.sh
+++ raspberry_setup.sh
@@ -17,6 +17,4 @@ systemctl unmask systemd-logind
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
-apt install cockpit
-
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) --stable-channel --disable-telemetry
