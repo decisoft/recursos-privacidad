c5525271385f61dbe6fdc1f272484b0d2b6385c4
Mario <72976464+decisoft@users.noreply.github.com>
2022-03-31 11:10:25 +0200

    Update raspberry_setup.sh

--- raspberry_setup.sh
+++ raspberry_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 apt update
-apt upgrade
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
@@ -12,13 +11,18 @@ apt-get install -y python3 python3-pip
 
 pip3 install docker-compose
 
-systemctl unmask systemd-logind
+# systemctl unmask systemd-logind
 
 groupadd docker
-usermod -aG docker dietpi
+usermod -aG docker mario
 newgrp docker
 
 docker volume create portainer_data
+docker network create gluetun
+docker network create books
+docker network create vaultwarden
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
-wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --disable-telemetry
+# wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --disable-telemetry
+
+apt upgrade
